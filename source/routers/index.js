01ef6dea9c41218b8d9b1765cb65a3adf32e8f55
codewithsenna <codewithsenna@gmail.com>
2025-03-03 20:05:35 -0500

    routers updated

--- source/routers/index.js
+++ source/routers/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Backend is working...')
-})
+    res.send('Backend is working...');
+});
 
 export default router;
+
+
+
